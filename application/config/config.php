99ce21a0c3bf5602d49ddb1b32aec09a560b9334
ademaulana12 <ademaulana464@gmail.com>
2021-08-22 02:29:46 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/demo/demo-8-klinik/';
+// $config['base_url'] = 'http://localhost/demo/demo-8-klinik/';
+$config['base_url'] = 'http://demo.mademaulana.skom.id/app/';
 
 /*
 |--------------------------------------------------------------------------
